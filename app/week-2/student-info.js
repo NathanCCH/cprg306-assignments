945f684b183b93d8b52b2f0def96c501d43c52ca
NathanCCH <nathan.cch@outlook.com>
2024-01-16 21:38:19 -0700

    Add StudentInfo component

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Nathan Hsieh</h1>
+            <Link href="https://github.com/NathanCCH?tab=repositories">GitHub</Link>
+        </main>
+    )
+}
