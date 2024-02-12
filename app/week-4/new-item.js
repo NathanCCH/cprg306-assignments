200133f143d83399b6de16c4a1b2c3a0a6470166
NathanCCH <nathan.cch@outlook.com>
2024-02-12 15:14:12 -0700

    Finalized page

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -14,7 +14,7 @@ export default function NetItem() {
       name,
       quantity,
       category,
-    }
+    };
 
     console.log(item);
 
@@ -23,27 +23,29 @@ export default function NetItem() {
     setName("");
     setQuantity(1);
     setCategory("produce");
-  }
+  };
 
   return (
-    <form>
-      <input type="text" placeholder="Item name" value={name} onchange={setName} required />
-      <input type="number" min="1" max="99" value={quantity} onchange={setQuantity} required />
-      <select onchange={setCategory}>
-        <option value="produce">Produce</option>
-        <option value="dairy">Dairy</option>
-        <option value="bakery">Bakery</option>
-        <option value="meat">Meat</option>
-        <option value="frozen_foods">Frozen Foods</option>
-        <option value="canned_goods">Canned Goods</option>
-        <option value="dry_goods">Dry Goods</option>
-        <option value="beverages">Beverages</option>
-        <option value="snacksoduce">Snacks</option>
-        <option value="household">Household</option>
-        <option value="other">Other</option>
-      </select>
-      <button onClick={handleSubmit}>Submit</button>
+    <form class="p-3 m-3 bg-slate-900 text-black rounded-md max-w-sm w-full">
+      <input type="text" placeholder="Item name" value={name} onChange={(e) => setName(e.target.value)} required class="w-full h-10 mb-3 pl-2 border-2 border-gray-400 rounded-lg focus:bg-yellow-100" />
+      <div class="flex justify-between mb-3">
+        <input type="number" min="1" max="99" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))} required class="h-10 pl-2 w-2/5 border-2 border-gray-400 focus:bg-yellow-100 rounded-lg" />
+        <select value={category} onChange={(e) => setCategory(e.target.value)} class="ml-2 w-3/5 h-10 pl-2 border-2 border-gray-400 focus:bg-yellow-100 rounded-lg">
+          <option value="produce">Produce</option>
+          <option value="dairy">Dairy</option>
+          <option value="bakery">Bakery</option>
+          <option value="meat">Meat</option>
+          <option value="frozen_foods">Frozen Foods</option>
+          <option value="canned_goods">Canned Goods</option>
+          <option value="dry_goods">Dry Goods</option>
+          <option value="beverages">Beverages</option>
+          <option value="snacks">Snacks</option>
+          <option value="household">Household</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+      <button onClick={handleSubmit} class="w-full h-10 text-white bg-blue-400 hover:bg-blue-500 rounded-lg ">Submit</button>
     </form>
-  )
+  );
 }
 
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -2,6 +2,8 @@ import NewItem from "./new-item";
 
 export default function Page() {
   return (
-    <NewItem />
+    <main class="flex justify-center w-full">
+      <NewItem />
+    </main>
   )
 }
