c4cce97add22d71c2d8ce613d0b5cb383399f4b8
NathanCCH <nathan.cch@outlook.com>
2024-02-27 16:46:48 -0700

    Add a link to week-5 on main page

--- app/page.js
+++ app/page.js
@@ -13,6 +13,9 @@ export default function Page() {
       <div>
         <Link href="week-4">Week 4</Link>
       </div>
+      <div>
+        <Link href="week-5">Week 5</Link>
+      </div>
     </main>
   );
 }
